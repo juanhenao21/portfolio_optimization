302bda3b190e035ece86726f50c7c8b32d22b5d9
juanhenao21 <juanhenao21@gmail.com>
2021-02-19 23:06:11 +0100

    Implement returns, normalized returns en corr matrix

--- project/correlation_matrix/correlation_matrix_analysis.py
+++ project/correlation_matrix/correlation_matrix_analysis.py
@@ -12,8 +12,8 @@ This script requires the following modules:
 
 The module contains the following functions:
     * returns - computes the returns of the time series.
-    * normalized_returns - normalize the returns of the time series.
-    * correlation_matrix - compute the correlation matrix of the normalized
+    * normalized_returns - normalizes the returns of the time series.
+    * correlation_matrix - computes the correlation matrix of the normalized
       returns.
     * main - the main function of the script.
 
@@ -34,8 +34,8 @@ import correlation_matrix_tools
 # -----------------------------------------------------------------------------
 
 
-def returns(year: str, time_step: str) -> None:
-    """computes the returns of the time series.
+def returns_data(year: str, time_step: str) -> None:
+    """Computes the returns of the time series.
 
     :param year: initial year of the analysis (i.e. '1980').
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
@@ -43,22 +43,22 @@ def returns(year: str, time_step: str) -> None:
      a value.
     """
 
-    function_name: str = returns.__name__
+    function_name: str = returns_data.__name__
     correlation_matrix_tools \
-        .function_header_print_data(function_name, year, 'returns')
+        .function_header_print_data(function_name, year, time_step)
 
     try:
 
         # Load data
-        data = pickle.load(open(
+        data: pd.DataFrame = pickle.load(open(
                     f'../data/original_data/original_data_{year}_step'
                     + f'_{time_step}.pickle', 'rb'))
 
-        data_returns = data.pct_change().dropna()
+        returns_df: pd.DataFrame = data.pct_change().dropna()
 
         # Saving data
         correlation_matrix_tools \
-            .save_data(data_returns, function_name, year, time_step)
+            .save_data(returns_df, function_name, year, time_step)
 
     except FileNotFoundError as error:
         print('No data')
@@ -68,73 +68,65 @@ def returns(year: str, time_step: str) -> None:
 # -----------------------------------------------------------------------------
 
 
-def hist_fx_correlations_physical_data(year: str, interval: str) -> None:
-    """Computes the correlation matrix in an interval of time.
+def normalized_returns_data(year: str, time_step: str) -> None:
+    """Normalizes the returns of the time series.
 
-    :param year: string of the year to be analyzed (i.e. '2016').
-    :param interval: string of the interval to be analyzed (i.e. 'week',
-     'month', 'quarter', 'year')
+    :param year: initial year of the analysis (i.e. '1980').
+    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the data in a file and does not return
      a value.
     """
 
-    function_name: str = hist_fx_correlations_physical_data.__name__
-    hist_data_tools_matrices_physical \
-        .hist_function_header_print_data(function_name, year, 'corr')
+    function_name: str = normalized_returns_data.__name__
+    correlation_matrix_tools \
+        .function_header_print_data(function_name, year, time_step)
 
     try:
+
         # Load data
-        fx_returns: pd.DataFrame = pickle.load(open(
-                        f'../../hist_data/matrices_physical_{year}/hist_fx'
-                        + f'_matrices_physical_data/hist_fx_returns_matrices'
-                        + f'_physical_data_{year}.pickle', 'rb'))
+        data: pd.DataFrame = pickle.load(open(
+                    f'../data/correlation_matrix/returns_data_{year}_step'
+                    + f'_{time_step}.pickle', 'rb'))
 
-        freq: str
-        periods: int
-        if interval == 'week':
-            freq = 'W'
-            periods = 52
-        elif interval == 'month':
-            freq = 'MS'
-            periods = 12
-        else:
-            freq = 'QS'
-            periods = 4
+        normalized_df: pd.DataFrame = (data - data.mean()) / data.std()
 
-        if interval == 'year':
-            corr: pd.DataFrame = fx_returns.corr()
-            hist_data_tools_matrices_physical \
-                .hist_save_data(corr, year, interval, '01')
+        # Saving data
+        correlation_matrix_tools \
+            .save_data(normalized_df, function_name, year, time_step)
 
-        else:
-            time_int: pd.DatetimeIndex = \
-                pd.date_range(f'{year}-01-01', periods=periods, freq=freq)
+    except FileNotFoundError as error:
+        print('No data')
+        print(error)
+        print()
 
-            t_idx: int
-            t_idx_str: str
-            for t_idx in range(1, periods):
-                corr = fx_returns[time_int[t_idx - 1]: time_int[t_idx]].corr()
+# -----------------------------------------------------------------------------
 
-                if t_idx < 10:
-                    t_idx_str = f'0{t_idx}'
-                else:
-                    t_idx_str = f'{t_idx}'
 
-                hist_data_tools_matrices_physical \
-                    .hist_save_data(corr, year, interval, t_idx_str)
+def correlation_matrix_data(year: str, time_step: str) -> None:
+    """Computes the correlation matrix of the normalized returns.
 
-            corr = fx_returns[time_int[-1]:].corr()
+    :param year: initial year of the analysis (i.e. '1980').
+    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
+    :return: None -- The function saves the data in a file and does not return
+     a value.
+    """
 
-            if t_idx < 10:
-                t_idx_str = f'0{t_idx + 1}'
-            else:
-                t_idx_str = f'{t_idx + 1}'
+    function_name: str = correlation_matrix_data.__name__
+    correlation_matrix_tools \
+        .function_header_print_data(function_name, year, time_step)
 
-            hist_data_tools_matrices_physical \
-                .hist_save_data(corr, year, interval, t_idx_str)
+    try:
 
-        del fx_returns
-        del corr
+        # Load data
+        data: pd.DataFrame = pickle.load(open(
+                    f'../data/correlation_matrix/normalized_returns_data'
+                    + f'_{year}_step_{time_step}.pickle', 'rb'))
+
+        corr_matrix_df: pd.DataFrame = data.corr()
+
+        # Saving data
+        correlation_matrix_tools \
+            .save_data(corr_matrix_df, function_name, year, time_step)
 
     except FileNotFoundError as error:
         print('No data')
