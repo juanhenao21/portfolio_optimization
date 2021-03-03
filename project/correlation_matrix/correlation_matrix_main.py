ee42a91d7acc6af30631e3d2c3f4426cc7ad2221
juanhenao21 <juanhenao21@gmail.com>
2021-03-03 13:41:26 +0100

    Implement data_plot_generator function

--- project/correlation_matrix/correlation_matrix_main.py
+++ project/correlation_matrix/correlation_matrix_main.py
@@ -19,10 +19,48 @@ The module contains the following functions:
 # Modules
 
 from typing import List
+import multiprocessing as mp
+from itertools import product as iprod
 
 import correlation_matrix_analysis
 import correlation_matrix_plot
 import correlation_matrix_tools
+
+# -----------------------------------------------------------------------------
+
+
+def data_plot_generator(years: List[str], time_step: List[str]) -> None:
+    """Generates all the analysis and plots from the data.
+    :param years: list of the string of the year to be analyzed
+     (i.e. ['2016', '2017']).
+    :param time_steps: list of the string of the time step of the data
+     (i.e. ['1m', '2m', '5m']).
+    :return: None -- The function saves the data in a file and does not return
+     a value.
+    """
+
+    # Parallel computing
+    with mp.Pool(processes=mp.cpu_count()) as pool:
+        # Specific functions
+        pool.starmap(correlation_matrix_analysis
+                     .returns_data, iprod(years, time_step))
+        pool.starmap(correlation_matrix_analysis
+                     .returns_data, iprod(years, time_step))
+        pool.starmap(correlation_matrix_analysis
+                     .normalized_returns_data, iprod(years, time_step))
+        pool.starmap(correlation_matrix_analysis
+                     .correlation_matrix_data, iprod(years, time_step))
+
+        # Plot
+        pool.starmap(correlation_matrix_plot
+                     .returns_plot, iprod(years, time_step))
+        pool.starmap(correlation_matrix_plot
+                     .normalized_returns_plot, iprod(years, time_step))
+        pool.starmap(correlation_matrix_plot
+                     .normalized_returns_distribution_plot,
+                     iprod(years, time_step))
+        pool.starmap(correlation_matrix_plot
+                     .correlation_matrix_plot, iprod(years, time_step))
 
 # -----------------------------------------------------------------------------
 
@@ -38,19 +76,15 @@ def main() -> None:
     correlation_matrix_tools.initial_message()
 
     # Initial year and time step
-    year = '1980'
-    time_step = '1d'
+    years: List[str] = ['1980']
+    time_steps: List[str] = ['1d']
 
     # Basic folders
-    correlation_matrix_tools.start_folders(year, time_step)
+    correlation_matrix_tools.start_folders()
 
     # Run analysis
     # Analysis and plot
-    correlation_matrix_analysis.returns_data(year, time_step)
-    correlation_matrix_analysis.normalized_returns_data(year, time_step)
-    correlation_matrix_analysis.correlation_matrix_data(year, time_step)
-
-    correlation_matrix_plot.returns_plot(year, time_step)
+    data_plot_generator(years, time_steps)
 
     print('Ay vamos!!!')
 
--- project/correlation_matrix/correlation_matrix_plot.py
+++ project/correlation_matrix/correlation_matrix_plot.py
@@ -57,27 +57,24 @@ def returns_plot(year: str, time_step: str) -> None:
                         f'../data/correlation_matrix/returns_data_{year}_step'
                         + f'_{time_step}.pickle', 'rb')).iloc[:, :5]
 
-        print(returns_data)
+        plot: np.ndarray = returns_data.plot(subplots=True, sharex=True,
+                                             figsize=(16, 16), grid=True,
+                                             sort_columns=True)
 
-        figure: plt.figure = plt.figure()
-
-        returns_data.plot(subplots=True, sharex=True, figsize=(16, 16), grid=True, xlim=(returns_data.index[0].year, returns_data.index[-1].year))
-
-        print()
-
-        # plt.xlabel('Time')#, fontsize=35)
-        # plt.ylabel('Returns')#, fontsize=35)
-        # plt.xticks(fontsize=25)
-        # plt.yticks(fontsize=25)
-        # plt.grid(True)
-        # plt.tight_layout()
-        plt.show()
+        _ = [ax.set_ylabel('Returns', fontsize=20) for ax in plot]
+        _ = [plot.legend(loc=1, fontsize=20) for plot in plt.gcf().axes]
+        plt.xlabel('Date', fontsize=20)
+        plt.tight_layout(0.5)
+        figure: plt.Figure = plot[0].get_figure()
 
         # Plotting
         correlation_matrix_tools \
             .save_plot(figure, function_name, year, time_step)
 
         plt.close()
+        del returns_data
+        del figure
+        gc.collect()
 
     except FileNotFoundError as error:
         print('No data')
@@ -88,7 +85,7 @@ def returns_plot(year: str, time_step: str) -> None:
 
 
 def normalized_returns_plot(year: str, time_step: str) -> None:
-    """Plots the returns of five stocks.
+    """Plots the normalized returns of five stocks.
 
     :param year: initial year of the analysis (i.e. '1980').
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
@@ -96,93 +93,91 @@ def normalized_returns_plot(year: str, time_step: str) -> None:
      a value.
     """
 
-    function_name: str = returns_plot.__name__
+    function_name: str = normalized_returns_plot.__name__
     correlation_matrix_tools \
         .function_header_print_plot(function_name, year, time_step)
 
     try:
 
         # Load data
-        returns_data: pd.DataFrame = pickle.load(open(
-                        f'../data/correlation_matrix/returns_data_{year}_step'
-                        + f'_{time_step}.pickle', 'rb')).head()
+        norm_returns_data: pd.DataFrame = pickle.load(open(
+            f'../data/correlation_matrix/normalized_returns_data_{year}_step'
+            + f'_{time_step}.pickle', 'rb')).iloc[:, :5]
 
-        print(returns_data)
+        plot: np.ndarray = norm_returns_data.plot(subplots=True, sharex=True,
+                                                  figsize=(16, 16), grid=True,
+                                                  sort_columns=True)
+
+        _ = [ax.set_ylabel('Norm. Returns', fontsize=20) for ax in plot]
+        _ = [plot.legend(loc=1, fontsize=20) for plot in plt.gcf().axes]
+        plt.xlabel('Date', fontsize=20)
+        plt.tight_layout(0.5)
+        figure: plt.Figure = plot[0].get_figure()
+
+        # Plotting
+        correlation_matrix_tools \
+            .save_plot(figure, function_name, year, time_step)
+
+        plt.close()
+        del norm_returns_data
+        del figure
+        gc.collect()
+
+    except FileNotFoundError as error:
+        print('No data')
+        print(error)
+        print()
+
+
+# -----------------------------------------------------------------------------
+
+
+def normalized_returns_distribution_plot(year: str, time_step: str) -> None:
+    """Plots the normalized returns distribution of five stocks.
+
+    :param year: initial year of the analysis (i.e. '1980').
+    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
+    :return: None -- The function saves the plot in a file and does not return
+     a value.
+    """
+
+    function_name: str = normalized_returns_distribution_plot.__name__
+    correlation_matrix_tools \
+        .function_header_print_plot(function_name, year, time_step)
+
+    try:
+
+        # Load data
+        norm_returns_data: pd.DataFrame = pickle.load(open(
+            f'../data/correlation_matrix/normalized_returns_data_{year}_step'
+            + f'_{time_step}.pickle', 'rb')).iloc[:, :5]
 
         x_gauss = np.arange(-5, 5, 0.001)
-        gaussian: np.ndarray = hist_data_tools_matrices_physical \
+        gaussian: np.ndarray = correlation_matrix_tools \
             .gaussian_distribution(0, 1, x_gauss)
 
-        figure: plt.figure = plt.figure(figsize=(16, 9))
+        plot = norm_returns_data.plot(kind='density', figsize=(16, 9))
 
-        if interval == 'year':
-            fx_returns_norm: pd.DataFrame = (fx_returns - fx_returns.mean()) \
-                                            / fx_returns.std()
+        plt.plot(x_gauss, gaussian, lw=5, label='Gaussian')
+        plt.title(f'Normalized returns distribution since {year}', fontsize=30)
+        plt.legend(loc=1, fontsize=20)
+        plt.xlabel('Returns', fontsize=25)
+        plt.ylabel('Counts', fontsize=25)
+        plt.xticks(fontsize=15)
+        plt.yticks(fontsize=15)
+        plt.xlim(-4, 4)
+        plt.grid(True)
+        plt.tight_layout()
+        figure: plt.Figure = plot.get_figure()
 
-            # pair: str
-            # for pair in fx_returns_norm.columns:
-            #     plt.hist(fx_returns_norm[pair], 100, (-4, 4), density=True)
-            #     # fx_returns_norm.plot(kind='kde')
-            fx_returns_norm.plot(kind='density')
+        # Plotting
+        correlation_matrix_tools \
+            .save_plot(figure, function_name, year, time_step)
 
-            # plt.plot(x_gauss, gaussian, lw=10, label='Gaussian')
-            plt.xlabel('Returns', fontsize=35)
-            plt.ylabel('Counts', fontsize=35)
-            plt.xticks(fontsize=25)
-            plt.yticks(fontsize=25)
-            plt.grid(True)
-            plt.tight_layout()
-
-            # Plotting
-            hist_data_tools_matrices_physical \
-                .hist_save_plot(function_name, figure, year, interval)
-
-            plt.close()
-            del fx_returns_norm
-            del figure
-            gc.collect()
-
-        # else:
-        #     time_int: pd.DatetimeIndex = \
-        #         pd.date_range(f'{year}-01-01', periods=periods, freq=freq)
-
-        #     t_idx: int
-        #     t_idx_str: str
-
-        #     for t_idx in range(1, periods):
-        #         fx_returns_period: pd.DataFrame = \
-        #             fx_returns[time_int[t_idx - 1]: time_int[t_idx]]
-        #         fx_returns_norm =  \
-        #             (fx_returns_period - fx_returns_period.mean()) \
-        #                 / fx_returns_period.std()
-
-        #         rename_df = {col: f'{col}_{interval}_{t_idx}' \
-        #             for col in fx_returns_norm.columns}
-        #         fx_returns_norm.rename(columns=rename_df, inplace=True)
-
-        #         fx_returns_norm.plot.kde()
-
-        #     plt.plot(x_gauss, gaussian, lw=10, label='Gaussian')
-        #     plt.xlabel('Returns', fontsize=35)
-        #     plt.ylabel('Counts', fontsize=35)
-        #     plt.xticks(fontsize=25)
-        #     plt.yticks(fontsize=25)
-        #     plt.grid(True)
-        #     plt.tight_layout()
-
-        #     if t_idx < 10:
-        #         t_idx_str = f'0{t_idx}'
-        #     else:
-        #         t_idx_str = f'{t_idx}'
-
-        #     # Plotting
-        #     hist_data_tools_matrices_physical \
-        #         .hist_save_data(function_name, figure, year, interval)
-
-        #     plt.close()
-        #     del fx_returns_norm
-        #     del figure
-        #     gc.collect()
+        plt.close()
+        del norm_returns_data
+        del figure
+        gc.collect()
 
     except FileNotFoundError as error:
         print('No data')
@@ -192,86 +187,42 @@ def normalized_returns_plot(year: str, time_step: str) -> None:
 # ----------------------------------------------------------------------------
 
 
-def hist_fx_correlations_physical_plot(year: str, interval: str) -> None:
-    """Plots the correlation matrix for different intervals of time.
+def correlation_matrix_plot(year: str, time_step: str) -> None:
+    """Plots the correlation matrix of the normalized returns.
 
-    :param year: string of the year to be analyzed (i.e. '2016').
-    :param interval: string of the interval to be analyzed (i.e. 'week',
-     'month', 'quarter', 'year')
+    :param year: initial year of the analysis (i.e. '1980').
+    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the plot in a file and does not return
      a value.
     """
 
-    function_name: str = \
-        hist_fx_correlations_physical_plot.__name__
-    hist_data_tools_matrices_physical \
-        .hist_function_header_print_plot(function_name, year, kind='correlations')
+    function_name: str = correlation_matrix_plot.__name__
+    correlation_matrix_tools \
+        .function_header_print_plot(function_name, year, time_step)
 
     try:
 
-        periods: int
-        n_cols: int
-        n_rows: int
-        if interval == 'week':
-            periods = 52
-            n_cols = 13
-            n_rows = 4
-        elif interval == 'month':
-            periods = 12
-            n_cols = 4
-            n_rows = 3
-        elif interval == 'quarter':
-            periods = 4
-            n_cols = 2
-            n_rows = 2
-        else:
-            periods = 1
-            n_cols = 1
-            n_rows = 1
-
         figure: plt.figure = plt.figure(figsize=(16, 9))
-        cbar_ax: plt.axes = figure.add_axes([0.91, 0.3, 0.03, 0.4])
 
-        for per in range(1, periods + 1):
+        # Load data
+        correlations: pd.DataFrame = pickle.load(open(
+            f'../data/correlation_matrix/correlation_matrix_data_{year}_step'
+            + f'_{time_step}.pickle', 'rb'))
 
-            if per < 10:
-                per_str: str = f'0{per}'
-            else:
-                per_str = f'{per}'
+        sns.heatmap(correlations, vmin=-1, vmax=1)
 
-            # Load data
-            corr: pd.DataFrame = pickle.load(open(
-                f'../../hist_data/matrices_physical_{year}/hist_fx_matrices'
-                + f'_physical_data/hist_fx_corr_physical_data_{year}_int'
-                + f'_{interval}_{per_str}.pickle', 'rb'))
+        plt.title(f'Correlation matrix since {year}', fontsize=30)
+        plt.yticks(rotation=45)
+        plt.xticks(rotation=45)
 
-            ax_sub = plt.subplot(n_rows, n_cols, per)
-
-            if interval in ('week', 'month'):
-                sns.heatmap(corr, ax=ax_sub, cbar=per == 1,
-                            cbar_ax=None if (per-1) else cbar_ax,
-                            vmin=-1, vmax=1)
-
-            else:
-                sns.heatmap(corr, annot=True, ax=ax_sub, cbar=per == 1,
-                            cbar_ax=None if (per-1) else cbar_ax,
-                            vmin=-1, vmax=1)
-
-            if interval == 'week':
-                ax_sub.tick_params(axis='x', bottom=False, labelbottom=False)
-                ax_sub.tick_params(axis='y', left=False, labelleft=False)
-
-            plt.yticks(rotation=45)
-            plt.xticks(rotation=45)
-
-        figure.tight_layout(rect=[0, 0, .9, 1])
+        figure.tight_layout()
 
         # Plotting
-        hist_data_tools_matrices_physical \
-            .hist_save_plot(function_name, figure, year, interval)
+        correlation_matrix_tools \
+            .save_plot(figure, function_name, year, time_step)
 
         plt.close()
-        del corr
+        del correlations
         del figure
         gc.collect()
 
@@ -290,8 +241,6 @@ def main() -> None:
 
     :return: None.
     """
-
-    hist_fx_returns_distributions_physical_plot('2019', 'year')
 
 # -----------------------------------------------------------------------------
 
