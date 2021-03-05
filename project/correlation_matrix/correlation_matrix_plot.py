2c9a28ed534d3dcbeac421345038e2f678efaad7
juanhenao21 <juanhenao21@gmail.com>
2021-03-05 12:59:41 +0100

    Plot volatility (standard deviation)

--- project/correlation_matrix/correlation_matrix_plot.py
+++ project/correlation_matrix/correlation_matrix_plot.py
@@ -6,6 +6,7 @@ correlation_matrix_analysis module.
 This script requires the following modules:
     * gc
     * pickle
+    * typing
     * matplotlib
     * numpy
     * pandas
@@ -14,6 +15,7 @@ This script requires the following modules:
 
 The module contains the following functions:
     * returns_plot - plots the returns of five stocks.
+    * volatility_plot - plots the volatility of five stocks.
     * normalized_returns_plot - plots the normalized returns of five stocks.
     * matrix_correlation_plot - plots the correlation matrix.
     * main - the main function of the script.
@@ -26,6 +28,7 @@ The module contains the following functions:
 
 import gc
 import pickle
+from typing import List
 
 from matplotlib import pyplot as plt  # type: ignore
 import numpy as np  # type: ignore
@@ -37,10 +40,11 @@ import correlation_matrix_tools
 # -----------------------------------------------------------------------------
 
 
-def returns_plot(year: str, time_step: str) -> None:
+def returns_plot(years: List[str], time_step: str) -> None:
     """Plots the returns of five stocks.
 
-    :param year: initial year of the analysis (i.e. '1980').
+    :param years: List of the interval of years to be analyzed
+     (i.e. ['1980', '2020']).
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the plot in a file and does not return
      a value.
@@ -48,14 +52,15 @@ def returns_plot(year: str, time_step: str) -> None:
 
     function_name: str = returns_plot.__name__
     correlation_matrix_tools \
-        .function_header_print_plot(function_name, year, time_step)
+        .function_header_print_plot(function_name, years, time_step)
 
     try:
 
         # Load data
         returns_data: pd.DataFrame = pickle.load(open(
-                        f'../data/correlation_matrix/returns_data_{year}_step'
-                        + f'_{time_step}.pickle', 'rb')).iloc[:, :5]
+                        f'../data/correlation_matrix/returns_data_{years[0]}'
+                        + f'_{years[1]}_step_{time_step}.pickle', 'rb')) \
+                        .iloc[:, :5]
 
         plot: np.ndarray = returns_data.plot(subplots=True, sharex=True,
                                              figsize=(16, 16), grid=True,
@@ -63,14 +68,15 @@ def returns_plot(year: str, time_step: str) -> None:
 
         _ = [ax.set_ylabel('Returns', fontsize=20) for ax in plot]
         _ = [plot.legend(loc=1, fontsize=20) for plot in plt.gcf().axes]
-        plt.title(f'Returns since {year} - {time_step}', fontsize=30)
+        plt.title(f'Returns from {years[0]} to {years[1]} - {time_step}',
+                  fontsize=30)
         plt.xlabel(f'Date - {time_step}', fontsize=20)
         plt.tight_layout(pad=0.5)
         figure: plt.Figure = plot[0].get_figure()
 
         # Plotting
         correlation_matrix_tools \
-            .save_plot(figure, function_name, year, time_step)
+            .save_plot(figure, function_name, years, time_step)
 
         plt.close()
         del returns_data
@@ -85,10 +91,63 @@ def returns_plot(year: str, time_step: str) -> None:
 # -----------------------------------------------------------------------------
 
 
-def normalized_returns_plot(year: str, time_step: str) -> None:
+def volatility_plot(years: List[str], time_step: str, window: str) -> None:
+    """Plots the volatility of five stocks.
+
+    :param years: List of the interval of years to be analyzed
+     (i.e. ['1980', '2020']).
+    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
+    :param window: window time to compute the volatility (i.e. '60', ...).
+    :return: None -- The function saves the plot in a file and does not return
+     a value.
+    """
+
+    function_name: str = volatility_plot.__name__
+    correlation_matrix_tools \
+        .function_header_print_plot(function_name, years, time_step)
+
+    try:
+
+        # Load data
+        volatility_data: pd.DataFrame = pickle.load(open(
+                        f'../data/correlation_matrix/volatility_data_win'
+                        + f'_{window}_{years[0]}_{years[1]}_step_{time_step}'
+                        + f'.pickle', 'rb')).iloc[:, :5]
+
+        plot: np.ndarray = volatility_data.plot(subplots=True, sharex=True,
+                                                figsize=(16, 16), grid=True,
+                                                sort_columns=True)
+
+        _ = [ax.set_ylabel('Volatility', fontsize=20) for ax in plot]
+        _ = [plot.legend(loc=1, fontsize=20) for plot in plt.gcf().axes]
+        plt.title(f'Volatility from {years[0]} to {years[1]} - {time_step}'
+                  + f' - time window {window}', fontsize=30)
+        plt.xlabel(f'Date - {time_step} - time window {window}', fontsize=20)
+        plt.tight_layout(pad=0.5)
+        figure: plt.Figure = plot[0].get_figure()
+
+        # Plotting
+        correlation_matrix_tools \
+            .save_plot(figure, function_name, years, time_step)
+
+        plt.close()
+        del volatility_data
+        del figure
+        gc.collect()
+
+    except FileNotFoundError as error:
+        print('No data')
+        print(error)
+        print()
+
+# -----------------------------------------------------------------------------
+
+
+def normalized_returns_plot(years: List[str], time_step: str) -> None:
     """Plots the normalized returns of five stocks.
 
-    :param year: initial year of the analysis (i.e. '1980').
+    :param years: List of the interval of years to be analyzed
+     (i.e. ['1980', '2020']).
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the plot in a file and does not return
      a value.
@@ -96,14 +155,14 @@ def normalized_returns_plot(year: str, time_step: str) -> None:
 
     function_name: str = normalized_returns_plot.__name__
     correlation_matrix_tools \
-        .function_header_print_plot(function_name, year, time_step)
+        .function_header_print_plot(function_name, years, time_step)
 
     try:
 
         # Load data
         norm_returns_data: pd.DataFrame = pickle.load(open(
-            f'../data/correlation_matrix/normalized_returns_data_{year}_step'
-            + f'_{time_step}.pickle', 'rb')).iloc[:, :5]
+            f'../data/correlation_matrix/normalized_returns_data_{years[0]}'
+            + f'_{years[1]}_step_{time_step}.pickle', 'rb')).iloc[:, :5]
 
         plot: np.ndarray = norm_returns_data.plot(subplots=True, sharex=True,
                                                   figsize=(16, 16), grid=True,
@@ -117,7 +176,7 @@ def normalized_returns_plot(year: str, time_step: str) -> None:
 
         # Plotting
         correlation_matrix_tools \
-            .save_plot(figure, function_name, year, time_step)
+            .save_plot(figure, function_name, years, time_step)
 
         plt.close()
         del norm_returns_data
@@ -133,10 +192,12 @@ def normalized_returns_plot(year: str, time_step: str) -> None:
 # -----------------------------------------------------------------------------
 
 
-def normalized_returns_distribution_plot(year: str, time_step: str) -> None:
+def normalized_returns_distribution_plot(years: List[str],
+                                         time_step: str) -> None:
     """Plots the normalized returns distribution of five stocks.
 
-    :param year: initial year of the analysis (i.e. '1980').
+    :param years: List of the interval of years to be analyzed
+     (i.e. ['1980', '2020']).
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the plot in a file and does not return
      a value.
@@ -144,26 +205,25 @@ def normalized_returns_distribution_plot(year: str, time_step: str) -> None:
 
     function_name: str = normalized_returns_distribution_plot.__name__
     correlation_matrix_tools \
-        .function_header_print_plot(function_name + 'lin', year, time_step)
+        .function_header_print_plot(function_name + 'lin', years, time_step)
 
     try:
 
         # Load data
         norm_returns_data: pd.DataFrame = pickle.load(open(
-            f'../data/correlation_matrix/normalized_returns_data_{year}_step'
-            + f'_{time_step}.pickle', 'rb')).iloc[:, :5]
+            f'../data/correlation_matrix/normalized_returns_data_{years[0]}'
+            + f'_{years[1]}_step_{time_step}.pickle', 'rb'))#.iloc[:, :5]
 
         x_gauss: np.ndarray = np.arange(-6, 6, 0.001)
         gaussian: np.ndarray = correlation_matrix_tools \
             .gaussian_distribution(0, 1, x_gauss)
 
         # Linear plot
-        plot = norm_returns_data.plot(kind='density',
-                                                   figsize=(16, 9))
+        plot = norm_returns_data.plot(kind='density', figsize=(16, 9))
 
         plt.plot(x_gauss, gaussian, lw=5, label='Gaussian')
-        plt.title(f'Normalized returns distribution since {year}'
-                  + f' - {time_step}', fontsize=30)
+        plt.title(f'Normalized returns distribution from {years[0]} to'
+                  + f' {years[1]} - {time_step}', fontsize=30)
         plt.legend(loc=1, fontsize=20)
         plt.xlabel('Returns', fontsize=25)
         plt.ylabel('Counts', fontsize=25)
@@ -176,7 +236,7 @@ def normalized_returns_distribution_plot(year: str, time_step: str) -> None:
 
         # Plotting
         correlation_matrix_tools \
-            .save_plot(figure, function_name + '_lin', year, time_step)
+            .save_plot(figure, function_name + '_lin', years, time_step)
 
         plt.close()
         del figure
@@ -186,10 +246,10 @@ def normalized_returns_distribution_plot(year: str, time_step: str) -> None:
         plt.hist(norm_returns_data, 100, (-5, 5), density=True, log=True,
                  histtype='step', label=norm_returns_data.columns)
         plt.plot(x_gauss, gaussian, lw=5, label='Gaussian')
-        plt.title(f'Normalized returns distribution since {year}'
-                  + f' - {time_step}', fontsize=30)
+        plt.title(f'Normalized returns distribution from {years[0]} to'
+                  + f' {years[1]} - {time_step}', fontsize=30)
 
-        plt.legend(loc=0, fontsize=20)
+        plt.legend(loc='lower center', fontsize=20)
         plt.xlabel('Returns', fontsize=25)
         plt.ylabel('Counts', fontsize=25)
         plt.xticks(fontsize=15)
@@ -200,7 +260,7 @@ def normalized_returns_distribution_plot(year: str, time_step: str) -> None:
 
         # Plotting
         correlation_matrix_tools \
-            .save_plot(figure, function_name + '_log', year, time_step)
+            .save_plot(figure, function_name + '_log', years, time_step)
 
         plt.close()
         del norm_returns_data
@@ -215,10 +275,11 @@ def normalized_returns_distribution_plot(year: str, time_step: str) -> None:
 # ----------------------------------------------------------------------------
 
 
-def correlation_matrix_plot(year: str, time_step: str) -> None:
+def correlation_matrix_plot(years: List[str], time_step: str) -> None:
     """Plots the correlation matrix of the normalized returns.
 
-    :param year: initial year of the analysis (i.e. '1980').
+    :param years: List of the interval of years to be analyzed
+     (i.e. ['1980', '2020']).
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the plot in a file and does not return
      a value.
@@ -226,7 +287,7 @@ def correlation_matrix_plot(year: str, time_step: str) -> None:
 
     function_name: str = correlation_matrix_plot.__name__
     correlation_matrix_tools \
-        .function_header_print_plot(function_name, year, time_step)
+        .function_header_print_plot(function_name, years, time_step)
 
     try:
 
@@ -234,13 +295,13 @@ def correlation_matrix_plot(year: str, time_step: str) -> None:
 
         # Load data
         correlations: pd.DataFrame = pickle.load(open(
-            f'../data/correlation_matrix/correlation_matrix_data_{year}_step'
-            + f'_{time_step}.pickle', 'rb'))
+            f'../data/correlation_matrix/correlation_matrix_data_{years[0]}'
+            + f'_{years[1]}_step_{time_step}.pickle', 'rb'))
 
         sns.heatmap(correlations, vmin=-1, vmax=1)
 
-        plt.title(f'Correlation matrix since {year} - {time_step}',
-                  fontsize=30)
+        plt.title(f'Correlation matrix from {years[0]} to {years[1]} -'
+                  + f' {time_step}', fontsize=30)
         plt.yticks(rotation=45)
         plt.xticks(rotation=45)
 
@@ -248,7 +309,7 @@ def correlation_matrix_plot(year: str, time_step: str) -> None:
 
         # Plotting
         correlation_matrix_tools \
-            .save_plot(figure, function_name, year, time_step)
+            .save_plot(figure, function_name, years, time_step)
 
         plt.close()
         del correlations
