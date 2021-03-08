50719b5a390e7bb5496578f98efddfe344cf2b76
juanhenao21 <juanhenao21@gmail.com>
2021-03-08 14:14:24 +0100

    Remove volatility function

--- project/correlation_matrix/correlation_matrix_analysis.py
+++ project/correlation_matrix/correlation_matrix_analysis.py
@@ -11,8 +11,6 @@ This script requires the following modules:
 
 The module contains the following functions:
     * returns_data - computes the returns of the time series.
-    * volatility_data - computes the volatility in a time window of the time
-     series.
     * normalized_returns_data - normalizes the returns of the time series.
     * correlation_matrix_data - computes the correlation matrix of the
      normalized returns.
@@ -33,11 +31,11 @@ import correlation_matrix_tools
 # -----------------------------------------------------------------------------
 
 
-def returns_data(years: List[str], time_step: str) -> None:
+def returns_data(dates: List[str], time_step: str) -> None:
     """Computes the returns of the time series.
 
-    :param years: List of the interval of years to be analyzed
-     (i.e. ['1980', '2020']).
+    :param dates: List of the interval of dates to be analyzed
+     (i.e. ['1980-01', '2020-12']).
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the data in a file and does not return
      a value.
@@ -45,20 +43,20 @@ def returns_data(years: List[str], time_step: str) -> None:
 
     function_name: str = returns_data.__name__
     correlation_matrix_tools \
-        .function_header_print_data(function_name, years, time_step)
+        .function_header_print_data(function_name, dates, time_step)
 
     try:
 
         # Load data
         data: pd.DataFrame = pickle.load(open(
-            f'../data/original_data/original_data_{years[0]}_{years[1]}_step'
+            f'../data/original_data/original_data_{dates[0]}_{dates[1]}_step'
             + f'_{time_step}.pickle', 'rb'))
 
         returns_df: pd.DataFrame = data.pct_change().dropna()
 
         # Saving data
         correlation_matrix_tools \
-            .save_data(returns_df, function_name, years, time_step)
+            .save_data(returns_df, function_name, dates, time_step)
 
     except FileNotFoundError as error:
         print('No data')
@@ -68,48 +66,11 @@ def returns_data(years: List[str], time_step: str) -> None:
 # -----------------------------------------------------------------------------
 
 
-def volatility_data(years: List[str], time_step: str, window: str) -> None:
-    """Computes the volatility in a time window of the time series.
-
-    :param years: List of the interval of years to be analyzed
-     (i.e. ['1980', '2020']).
-    :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
-    :param window: window time to compute the volatility (i.e. '60', ...).
-    :return: None -- The function saves the data in a file and does not return
-     a value.
-    """
-
-    function_name: str = volatility_data.__name__
-    correlation_matrix_tools \
-        .function_header_print_data(function_name, years, time_step)
-
-    try:
-
-        # Load data
-        data: pd.DataFrame = pickle.load(open(
-            f'../data/correlation_matrix/returns_data_{years[0]}_{years[1]}'
-            + f'_step_{time_step}.pickle', 'rb'))
-
-        std_df: pd.DataFrame = data.rolling(window=int(window)).std().dropna()
-
-        # Saving data
-        correlation_matrix_tools \
-            .save_data(std_df, function_name + f'_win_{window}', years,
-                       time_step)
-
-    except FileNotFoundError as error:
-        print('No data')
-        print(error)
-        print()
-
-# -----------------------------------------------------------------------------
-
-
-def normalized_returns_data(years: List[str], time_step: str) -> None:
+def normalized_returns_data(dates: List[str], time_step: str) -> None:
     """Normalizes the returns of the time series.
 
-    :param years: List of the interval of years to be analyzed
-     (i.e. ['1980', '2020']).
+    :param dates: List of the interval of dates to be analyzed
+     (i.e. ['1980-01', '2020-12']).
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the data in a file and does not return
      a value.
@@ -117,20 +78,20 @@ def normalized_returns_data(years: List[str], time_step: str) -> None:
 
     function_name: str = normalized_returns_data.__name__
     correlation_matrix_tools \
-        .function_header_print_data(function_name, years, time_step)
+        .function_header_print_data(function_name, dates, time_step)
 
     try:
 
         # Load data
         data: pd.DataFrame = pickle.load(open(
-            f'../data/correlation_matrix/returns_data_{years[0]}_{years[1]}'
+            f'../data/correlation_matrix/returns_data_{dates[0]}_{dates[1]}'
             + f'_step_{time_step}.pickle', 'rb'))
 
         normalized_df: pd.DataFrame = (data - data.mean()) / data.std()
 
         # Saving data
         correlation_matrix_tools \
-            .save_data(normalized_df, function_name, years, time_step)
+            .save_data(normalized_df, function_name, dates, time_step)
 
     except FileNotFoundError as error:
         print('No data')
@@ -140,11 +101,11 @@ def normalized_returns_data(years: List[str], time_step: str) -> None:
 # -----------------------------------------------------------------------------
 
 
-def correlation_matrix_data(years: List[str], time_step: str) -> None:
+def correlation_matrix_data(dates: List[str], time_step: str) -> None:
     """Computes the correlation matrix of the normalized returns.
 
-    :param years: List of the interval of years to be analyzed
-     (i.e. ['1980', '2020']).
+    :param dates: List of the interval of dates to be analyzed
+     (i.e. ['1980-01', '2020-12']).
     :param time_step: time step of the data (i.e. '1m', '2m', '5m', ...).
     :return: None -- The function saves the data in a file and does not return
      a value.
@@ -152,20 +113,20 @@ def correlation_matrix_data(years: List[str], time_step: str) -> None:
 
     function_name: str = correlation_matrix_data.__name__
     correlation_matrix_tools \
-        .function_header_print_data(function_name, years, time_step)
+        .function_header_print_data(function_name, dates, time_step)
 
     try:
 
         # Load data
         data: pd.DataFrame = pickle.load(open(
-            f'../data/correlation_matrix/normalized_returns_data_{years[0]}'
-            + f'_{years[1]}_step_{time_step}.pickle', 'rb'))
+            f'../data/correlation_matrix/normalized_returns_data_{dates[0]}'
+            + f'_{dates[1]}_step_{time_step}.pickle', 'rb'))
 
         corr_matrix_df: pd.DataFrame = data.corr()
 
         # Saving data
         correlation_matrix_tools \
-            .save_data(corr_matrix_df, function_name, years, time_step)
+            .save_data(corr_matrix_df, function_name, dates, time_step)
 
     except FileNotFoundError as error:
         print('No data')
@@ -173,7 +134,7 @@ def correlation_matrix_data(years: List[str], time_step: str) -> None:
         print()
 
     except TypeError as error:
-        print('To compute the correlation is needed at least to stocks')
+        print('To compute the correlation is needed at least two stocks')
         print(error)
         print()
 
